2521a5595f1e9286fe6f44119f6872b943a4ab23
Anshika Yadav <anshika361436@gmail.com>
2021-07-19 20:12:56 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const https=require("https");
+var app=express();
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res)
+{
+    res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res)
+{
+    const firstName=req.body.firstName;
+    const lastName=req.body.lastName;
+    const email=req.body.email;
+    console.log(firstName);
+    console.log(lastName);
+    console.log(email);
+    const data={
+        members:[
+            {
+            email_address:email,
+            status:"subscribed",
+            merge_fields: {
+                FNAME:firstName,
+                LNAME:lastName
+            }
+            }
+        ]
+    };
+    const jsonData=JSON.stringify(data);
+    const url="https://us6.api.mailchimp.com/3.0/lists/9973f2d79f";
+    const options =
+    {
+        method: "POST",
+        auth:"anshuka :88a4587f288660ffaca7a164381b5647-us6",
+
+
+    };
+
+
+    const request=  https.request(url,options,function(response)
+    {
+        if(response.statusCode===200)
+        {
+            res.sendFile(__dirname+"/success.html");
+        }
+        else
+        {
+            res.sendFile(__dirname+"/failure.html");
+        }
+    //   response.on("data",function(data)
+    //   {
+    //       console.log(JSON.parse(data));
+    //   });
+    });
+    request.write(jsonData);
+    request.end();
+
+});
+app.post("/failure",function(req,res)
+{
+    res.redirect("/");
+});
+
+
+
+
+
+
+app.listen(3000,function(){
+    console.log("server is running on port 3000");
+
+});
